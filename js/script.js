a02c1a5e42a3ca36954e4e303d6147e7ac910bf4
Lau <arinel88@gmail.com>
2020-08-26 01:21:36 +0200

    Finita parte js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Chiedo all'utente il suo cognome
+var cognomeUser = prompt("Ciao! Inserisci qui il tuo cognome");
+cognomeUser = cognomeUser.charAt(0).toUpperCase() + cognomeUser.slice(1);
+
+// Lo inserisco nell'array
+
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+cognomi.push(cognomeUser);
+
+// Ordino alfabeticamente l'array
+
+cognomi.sort();
+console.log(cognomi);
+
+// Stampo la lista dei cognomi
+for (var i = 0; i < cognomi.length; i++){
+  var listaCognomi = document.getElementById('lista').innerHTML; //lettura
+  document.getElementById('lista').innerHTML = listaCognomi + "<li>" + cognomi[i] + "</li>";
+}
+
+// Scrivo in che posizione si trova il cognome inserito dall'utente
+
+console.log(cognomi.indexOf(cognomeUser) + 1);
